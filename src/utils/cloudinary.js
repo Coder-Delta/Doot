b3b4eaeb1d3204efce53096065b1505d554a75b9
Coder-Delta <ranjitbhandary15@gmail.com>
2025-12-11 11:58:02 +0530

    feat: implement JWT authentication and user model; add error handling utilities and cloudinary integration

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,35 @@
+import { v2 as cloudinary } from "cloudinary"; //in here "as" coustomize the v2 to cloudnary
+import fs from "fs";
+
+
+//algo=> 
+    //config karo
+    //upload karo
+    //file delete karo
+    //agar nahoea to error do
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
+});
+
+
+const uploadOnCloudinary = async (localFilepath) => {
+    try {
+        if (!localFilepath) return null
+        //upload file on cloudenary
+        const response = await cloudinary.uploader.upload(localFilepath,{
+            resource_type: "auto",
+        })
+        //file has been uploaded successfull
+        // console.log("File uploaded on cloudinary", response.url);
+        fs.unlinkSync(localFilepath)
+        return response
+        
+    } catch (error) {
+        fs.unlinkSync(localFilepath)//remove the locally saved temp file as upload operation got failed
+        return null
+    }
+}
+
+export {uploadOnCloudinary}
