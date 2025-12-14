385c82f1398087b165429d2dd33123b45f9c22a1
Coder-Delta <ranjitbhandary15@gmail.com>
2025-12-14 13:31:15 +0530

    Refactor user-related modules: update imports, enhance CORS setup, and streamline password hashing logic

--- src/app.js
+++ src/app.js
@@ -1,5 +1,34 @@
 import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
-const app =express()
+const app = express()
 
-export {app}
+// ---------------------- CORS ----------------------
+app.use(cors({
+    origin: process.env.CORS_ORIGIN || process.env.CROS_ORIGIN,
+    credentials: true,
+}))
+
+
+// ---------------------- Middlewares ----------------------
+
+app.use(express.json({ limit: "16kb" }))
+
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
+
+app.use(express.static("public"))
+
+app.use(cookieParser())
+
+// ---------------------- Routes Import ----------------------
+
+import userRouter from "./routes/user.route.js"
+
+// ---------------------- Routes Declaration ----------------------
+
+app.use("/api/v1/users", userRouter)
+
+
+// ---------------------- Export ----------------------
+export { app }
